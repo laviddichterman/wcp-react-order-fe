fc057b28706e201f9f3b505c1e5a8718f96f3e7a
Dave Lichterman <me@lavid.me>
2024-02-20 18:20:27 -0800

    v 0.4.1 main category tree ordering
    * merge WShopForSuppProductsStage & WShopForPrimaryProductsStage and handle both cases

--- src/components/step/WShopForProductsStageComponent.tsx
+++ src/components/step/WShopForProductsStageComponent.tsx
@@ -2,32 +2,55 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Accordion, AccordionSummary, AccordionDetails, Grid, Typography } from '@mui/material';
 import { ExpandMore } from "@mui/icons-material";
 import { ClickableProductDisplay } from '../WProductComponent';
-import { IProductInstance } from '@wcp/wcpshared';
+import { FilterEmptyCategories, FilterProduct, IMenu, IProductInstance } from '@wcp/wcpshared';
 import { useAppSelector } from '../../app/useHooks';
 import { SelectServiceDateTime } from '../../app/slices/WFulfillmentSlice';
-import { FilterEmptyCategoriesWrapper, WShopForProductsStageProps } from './WShopForProductsStageContainer';
-import { Separator, StageTitle, scrollToElementOffsetAfterDelay } from '@wcp/wario-ux-shared';
+import { WShopForProductsStageProps } from './WShopForProductsStageContainer';
+import { scrollToElementOffsetAfterDelay } from '@wcp/wario-ux-shared';
 
-export function WShopForSuppProductsStage({ categoryId, ProductsForCategoryFilteredAndSorted, onProductSelection, hidden }: WShopForProductsStageProps) {
+export interface OrderHideable {
+  order: {
+    hide: boolean;
+  };
+}
+// NOTE: any calls to this are going to need the order_time properly piped because right now it's just getting the fulfillment.dt.day
+export const FilterProductWrapper = function <T extends OrderHideable>(menu: IMenu, order_time: Date | number, fulfillmentId: string) {
+  return (item: IProductInstance) => FilterProduct(item, menu, (x: T) => x.order.hide, order_time, fulfillmentId)
+};
+
+export const FilterEmptyCategoriesWrapper = function <T extends OrderHideable>(menu: IMenu, order_time: Date | number, fulfillmentId: string) {
+  return FilterEmptyCategories(menu, (x: T) => x.order.hide, order_time, fulfillmentId);
+};
+
+export const ProductsForCategoryFilteredAndSortedFxnGen = function (menu: IMenu | null, serviceDateTime: Date | null, fulfillmentId: string) {
+  return serviceDateTime !== null && menu !== null ?
+    ((category: string) => menu.categories[category].menu.filter(FilterProductWrapper(menu, serviceDateTime, fulfillmentId)).sort((p) => p.displayFlags.order.ordinal)) :
+    ((_: string) => [])
+}
+
+
+export function WShopForProductsStage({ categoryId, onProductSelection }: WShopForProductsStageProps) {
   // TODO: we need to handle if this is null by choice. how to we bypass this stage?
   const menu = useAppSelector(s => s.ws.menu!);
-  const selectedService = useAppSelector(s=>s.fulfillment.selectedService);
+  const selectedService = useAppSelector(s => s.fulfillment.selectedService!);
   const serviceDateTime = useAppSelector(s => SelectServiceDateTime(s.fulfillment));
   const [activePanel, setActivePanel] = useState(0);
   const [isExpanded, setIsExpanded] = useState(true);
-  const [extrasCategories, setExtrasCategories] = useState<string[]>([]);
-
+  const [subCategories, setSubCategories] = useState<string[]>([]);
+  const ProductsForCategoryFilteredAndSorted = useCallback((category: string) =>
+    ProductsForCategoryFilteredAndSortedFxnGen(menu, serviceDateTime, selectedService)(category),
+    [menu, serviceDateTime, selectedService]);
 
   // reinitialize the accordion if the expanded is still in range 
   useEffect(() => {
     if (serviceDateTime !== null && selectedService !== null && categoryId !== null) {
       const extras = menu.categories[categoryId].children.length ? menu.categories[categoryId].children.filter(FilterEmptyCategoriesWrapper(menu, serviceDateTime, selectedService)) : [];
-      if (extras.length !== extrasCategories.length) {
+      if (extras.length !== subCategories.length) {
         setActivePanel(0);
-        setExtrasCategories(extras);
+        setSubCategories(extras);
       }
     }
-  }, [categoryId, extrasCategories.length, serviceDateTime, menu, selectedService]);
+  }, [categoryId, subCategories.length, serviceDateTime, menu, selectedService]);
 
   const toggleAccordion = useCallback((event: React.SyntheticEvent<Element, Event>, i: number) => {
     event.preventDefault();
@@ -45,10 +68,23 @@ export function WShopForSuppProductsStage({ categoryId, ProductsForCategoryFilte
   }, [activePanel, isExpanded]);
 
   return (
-    <div hidden={hidden}>
-      <StageTitle>Add small plates and other stuff to your order.</StageTitle>
-      <Separator sx={{ pb: 3 }} />
-      {extrasCategories.map((catId, i) =>
+    <>
+      <Grid container>
+        {ProductsForCategoryFilteredAndSorted(categoryId).map((p: IProductInstance, i: number) =>
+          <Grid item xs={12} md={6} lg={4} xl={3} key={i} >
+            <ClickableProductDisplay
+              sx={{ mb: 3.75, mx: 2 }}
+              onClick={(e) => onProductSelection('WARIO_order', categoryId, p.id)}
+              productMetadata={menu.product_instance_metadata[p.id]}
+              allowAdornment
+              description
+              dots
+              price
+              displayContext="order"
+            />
+          </Grid>)}
+      </Grid>
+      {subCategories.map((catId, i) =>
         <Accordion id={`accordion-${catId}`} key={i} expanded={activePanel === i && isExpanded} onChange={(e) => toggleAccordion(e, i)} >
           <AccordionSummary expandIcon={activePanel === i && isExpanded ? <ExpandMore /> : <ExpandMore />}>
             <Typography variant='h5' sx={{ ml: 4 }}><span dangerouslySetInnerHTML={{ __html: menu!.categories[catId].menu_name }} /></Typography>
@@ -74,6 +110,6 @@ export function WShopForSuppProductsStage({ categoryId, ProductsForCategoryFilte
             </Grid>
           </AccordionDetails>
         </Accordion>)}
-    </div>
+    </>
   );
 }
--- src/components/step/WShopForProductsStageContainer.tsx
+++ src/components/step/WShopForProductsStageContainer.tsx
@@ -1,45 +1,22 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useSnackbar } from 'notistack';
-import { CartEntry, WProduct, FilterProduct, IMenu, IProductInstance, FilterEmptyCategories, CreateWCPProduct } from '@wcp/wcpshared';
+import { CartEntry, WProduct, CreateWCPProduct } from '@wcp/wcpshared';
 import { customizeProduct, editCartEntry } from '../../app/slices/WCustomizerSlice';
 import { useAppDispatch, useAppSelector } from '../../app/useHooks';
 import { WProductCustomizerComponent } from '../WProductCustomizerComponent';
 import { GetSelectableModifiers, SelectMainCategoryId, SelectMainProductCategoryCount, selectSelectedProduct, SelectSupplementalCategoryId } from '../../app/store';
 import { getCart, updateCartQuantity, addToCart, FindDuplicateInCart, lockCartEntry } from '../../app/slices/WCartSlice';
-import { SelectServiceDateTime } from '../../app/slices/WFulfillmentSlice';
 import { nextStage, backStage } from '../../app/slices/StepperSlice';
 import { Navigation } from '../Navigation';
 import { WOrderCart } from '../WOrderCartComponent';
-import { WShopForPrimaryProductsStage } from './WShopForPrimaryProductsStageComponent';
-import { WShopForSuppProductsStage } from './WShopForSuppProductsStageComponent';
+import { WShopForProductsStage } from './WShopForProductsStageComponent';
 import { cloneDeep } from 'lodash';
 import { setTimeToFirstProductIfUnset } from '../../app/slices/WMetricsSlice';
-import { Separator, CatalogSelectors, getProductEntryById, getProductInstanceById, scrollToIdOffsetAfterDelay } from '@wcp/wario-ux-shared';
+import { Separator, CatalogSelectors, getProductEntryById, getProductInstanceById, scrollToIdOffsetAfterDelay, StageTitle } from '@wcp/wario-ux-shared';
 
-export interface OrderHideable { 
-  order: {
-    hide: boolean;
-  };
-}
-// NOTE: any calls to this are going to need the order_time properly piped because right now it's just getting the fulfillment.dt.day
-export const FilterProductWrapper = function <T extends OrderHideable>(menu: IMenu, order_time: Date | number, fulfillmentId: string) {
-  return (item: IProductInstance) => FilterProduct(item, menu, (x: T) => x.order.hide, order_time, fulfillmentId)
-};
-
-export const FilterEmptyCategoriesWrapper = function <T extends OrderHideable>(menu: IMenu, order_time: Date | number, fulfillmentId: string) {
-  return FilterEmptyCategories(menu, (x: T) => x.order.hide, order_time, fulfillmentId);
-};
-
-export const ProductsForCategoryFilteredAndSortedFxnGen = function (menu: IMenu | null, serviceDateTime: Date | null, fulfillmentId: string) {
-  return serviceDateTime !== null && menu !== null ?
-    ((category: string) => menu.categories[category].menu.filter(FilterProductWrapper(menu, serviceDateTime, fulfillmentId)).sort((p) => p.displayFlags.order.ordinal)) :
-    ((_: string) => [])
-}
 export interface WShopForProductsStageProps {
   categoryId: string;
-  ProductsForCategoryFilteredAndSorted: (category: string) => IProductInstance[];
   onProductSelection: (returnToId: string, cid: string, pid: string) => void;
-  hidden: boolean;
 }
 
 export function WShopForProductsContainer({ productSet }: { productSet: 'PRIMARY' | 'SECONDARY' }) {
@@ -47,19 +24,18 @@ export function WShopForProductsContainer({ productSet }: { productSet: 'PRIMARY
   const numMainCategoryProducts = useAppSelector(SelectMainProductCategoryCount);
   const mainCategoryId = useAppSelector(SelectMainCategoryId)!;
   const supplementalCategoryId = useAppSelector(SelectSupplementalCategoryId);
-  const catalogSelectors = useAppSelector(s=>CatalogSelectors(s.ws));
-  const selectedService = useAppSelector(s=>s.fulfillment.selectedService!);
+  const catalogSelectors = useAppSelector(s => CatalogSelectors(s.ws));
   const menu = useAppSelector(s => s.ws.menu!);
   const { enqueueSnackbar } = useSnackbar();
   const productEntrySelector = useAppSelector(s => (id: string) => getProductEntryById(s.ws.products, id));
   const productInstanceSelector = useAppSelector(s => (id: string) => getProductInstanceById(s.ws.productInstances, id));
   const cart = useAppSelector(s => getCart(s.cart.cart));
-  const serviceDateTime = useAppSelector(s => SelectServiceDateTime(s.fulfillment));
   const selectedProduct = useAppSelector(selectSelectedProduct);
   const dispatch = useAppDispatch();
-  const ProductsForCategoryFilteredAndSorted = useCallback((category: string) =>
-    ProductsForCategoryFilteredAndSortedFxnGen(menu, serviceDateTime, selectedService)(category),
-    [menu, serviceDateTime, selectedService]);
+  const titleString = useMemo(() => productSet === 'PRIMARY' ?
+    (numMainCategoryProducts > 0 ? "Click a pizza below or next to continue." : "Click a pizza below to get started.") :
+    'Add small plates and other stuff to your order.',
+    [productSet, numMainCategoryProducts]);
 
 
   const onProductSelection = useCallback((returnToId: string, cid: string, pid: string) => {
@@ -103,9 +79,19 @@ export function WShopForProductsContainer({ productSet }: { productSet: 'PRIMARY
 
   return (
     <div>
-      { productSet === 'PRIMARY' ?
-          <WShopForPrimaryProductsStage categoryId={mainCategoryId} hidden={selectedProduct !== null} onProductSelection={onProductSelection} ProductsForCategoryFilteredAndSorted={ProductsForCategoryFilteredAndSorted} /> :
-          <WShopForSuppProductsStage categoryId={supplementalCategoryId!} hidden={selectedProduct !== null} onProductSelection={onProductSelection} ProductsForCategoryFilteredAndSorted={ProductsForCategoryFilteredAndSorted} />}
+      <div hidden={selectedProduct !== null}>
+        <StageTitle>{titleString}</StageTitle>
+        <Separator sx={{ pb: 3 }} />
+        {productSet === 'PRIMARY' ?
+          <WShopForProductsStage
+            categoryId={mainCategoryId}
+            onProductSelection={onProductSelection}
+          /> :
+          <WShopForProductsStage
+            categoryId={supplementalCategoryId!}
+            onProductSelection={onProductSelection}
+          />}
+      </div>
       {selectedProduct !== null && (<WProductCustomizerComponent scrollToWhenDone={scrollToOnReturn} />)}
       {cart.length > 0 && <Separator />}
       <WOrderCart isProductEditDialogOpen={selectedProduct !== null} setProductToEdit={setProductToEdit} />
